67c5fb9a3e5deda6332bbc6aae6ed0817821ca42
computershawn <shontek@gmail.com>
2019-07-27 15:12:29 -0700

    Switch to using the JWT auth backend

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react'
 import TokenService from '../../services/token-service'
+import AuthApiService from '../../services/auth-api-service'
 import { Button, Input } from '../Utils/Utils'
 
 export default class LoginForm extends Component {
   static defaultProps = {
-    onLoginSuccess: () => {}
+    onLoginSuccess: () => { }
   }
 
   state = { error: null }
 
-  handleSubmitBasicAuth = ev => {
+  // handleSubmitBasicAuth = ev => {
+  //   ev.preventDefault()
+  //   const { user_name, password } = ev.target
+
+  //   // console.log('login form submitted')
+  //   console.log({ user_name, password })
+
+  //   TokenService.saveAuthToken(
+  //     TokenService.makeBasicAuthToken(user_name.value, password.value)
+  //   )
+
+  //   user_name.value = ''
+  //   password.value = ''
+  //   this.props.onLoginSuccess()
+  // }
+  handleSubmitJwtAuth = ev => {
     ev.preventDefault()
+    this.setState({ error: null })
     const { user_name, password } = ev.target
 
-    // console.log('login form submitted')
-    console.log({ user_name, password })
-
-    TokenService.saveAuthToken(
-      TokenService.makeBasicAuthToken(user_name.value, password.value)
-    )
-
-    user_name.value = ''
-    password.value = ''
-    this.props.onLoginSuccess()
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value,
+    })
+      .then(res => {
+        user_name.value = ''
+        password.value = ''
+        TokenService.saveAuthToken(res.authToken)
+        this.props.onLoginSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
   }
 
   render() {
@@ -30,7 +50,7 @@ export default class LoginForm extends Component {
     return (
       <form
         className='LoginForm'
-        onSubmit={this.handleSubmitBasicAuth}
+        onSubmit={this.handleSubmitJwtAuth}
       >
         <div role='alert'>
           {error && <p className='red'>{error}</p>}
